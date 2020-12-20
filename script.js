8694da7950ee749594014f69b6202697cb2c7f12
nahilnaceri <chafikmihoubnaceri@gmail.com>
2020-12-20 20:39:36 +0100

    score function

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+// document.querySelector('.guess').addEventListener('change', () => {
+//   if (typeof this.value !== 'number') {
+//     alert('Please type in a number');
+//   }
+// });
+
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+
+let myScore = Number(document.querySelector('.score').textContent);
+
+document.querySelector('.check').addEventListener('click', e => {
+  const guess = Number(document.querySelector('.guess').value);
+  let myString = ``;
+  if (!guess) {
+    myString = '🚫 Not a valid number!';
+  } else if (guess === secretNumber) {
+    myString = 'You Found My Number';
+    document.querySelector('.number').textContent = secretNumber;
+  } else if (guess > secretNumber) {
+    myString = 'Too High';
+    myScore -= 1;
+  } else {
+    myString = 'Too Low';
+    myScore -= 1;
+  }
+
+  document.querySelector('.message').textContent = myString;
+  document.querySelector('.score').textContent = myScore;
+});
