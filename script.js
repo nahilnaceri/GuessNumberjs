d22e31367bcc2eceba7fd95216fdaeef75a51c8d
nahilnaceri <chafikmihoubnaceri@gmail.com>
2020-12-20 20:54:12 +0100

    winner or loser

--- script.js
+++ script.js
@@ -13,17 +13,23 @@ let myScore = Number(document.querySelector('.score').textContent);
 document.querySelector('.check').addEventListener('click', e => {
   const guess = Number(document.querySelector('.guess').value);
   let myString = ``;
-  if (!guess) {
+  if (myScore <= 1) {
+    myString = `You Lose`;
+    document.querySelector('body').style.backgroundColor = 'red';
+  } else if (!guess) {
     myString = '🚫 Not a valid number!';
   } else if (guess === secretNumber) {
     myString = 'You Found My Number';
     document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
   } else if (guess > secretNumber) {
     myString = 'Too High';
     myScore -= 1;
+    //document.querySelector('.guess').value = '';
   } else {
     myString = 'Too Low';
     myScore -= 1;
+    //document.querySelector('.guess').value = '';
   }
 
   document.querySelector('.message').textContent = myString;
